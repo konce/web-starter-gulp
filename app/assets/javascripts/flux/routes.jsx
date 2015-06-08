7fbf409df333097d5e759ab931447adddf833586
Iuriy-Budnikov <iuriy.budnikov@abcsolute.com>
2015-06-08 12:32:03 +0300

    React

--- app/assets/javascripts/flux/routes.jsx
+++ app/assets/javascripts/flux/routes.jsx
@@ -1,13 +1,15 @@
 "use strict";
 
-import React from "react"
+import React from "react/addons"
 import Router from "react-router"
 import Layout from "./layout"
 import Home from "./home/home"
 import Posts from "./posts/posts"
 
 var Route = Router.Route,
-    DefaultRoute = Router.DefaultRoute;
+    Location = Router.Route,
+    DefaultRoute = Router.DefaultRoute,
+    CSSTransitionGroup = React.addons.CSSTransitionGroup;
 
 export default Route =
   <Route handler={Layout} name="home" path="/">
